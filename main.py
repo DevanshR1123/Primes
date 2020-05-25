9fcec1f380cf3f1628aeebf46f9e2addc41a2232
DevanshR1123 <65907921+DevanshR1123@users.noreply.github.com>
2020-05-25 22:55:46 +0530

    Update and rename gen_primes.py to main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from math import sqrt, ceil
-from time import time
 
 primes = [2, 3]
 
 
-def is_prime(x): return all(map(lambda n: x % n != 0,
-                                filter(lambda n: n < ceil(sqrt(x)), primes)))
-
+def is_prime(x): return all(map(lambda n: x % n != 0,filter(lambda n: n < ceil(sqrt(x)), primes)))
 
 def gen_primes(n):
     possible_primes = sorted(list(map(lambda x: 6*x + 1, range(1, n//2+1))) +
@@ -14,11 +11,6 @@ def gen_primes(n):
     p_primes = filter(is_prime, possible_primes)
     primes.extend(p_primes)
 
-
-start = time()
 gen_primes(1000000)
-end = time()
-
-print(f'Generated {len(primes)} Primes in {end - start: .3f}')
 
 open('primes', 'w+').write('\n'.join(map(str, primes)))
