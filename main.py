4421e7619fd1c23b6f2a3c64685a1b1c001fd57d
DevanshR1123 <65907921+DevanshR1123@users.noreply.github.com>
2020-05-26 18:32:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ primes = [2, 3]
 def is_prime(x): return all(map(lambda n: x % n != 0,filter(lambda n: n < ceil(sqrt(x)), primes)))
 
 def gen_primes(n):
-    possible_primes = sorted(list(map(lambda x: 6*x + 1, range(1, n//2+1))) +
-                             list(map(lambda x: 6*x - 1, range(1, n//2+1))))
+    possible_primes = sorted(list(map(lambda x: 6*x + 1, range(1, n+1))) +
+                             list(map(lambda x: 6*x - 1, range(1, n+1))))
     p_primes = filter(is_prime, possible_primes)
     primes.extend(p_primes)
 
-gen_primes(1000000)
+gen_primes(1000000000)
 
 open('primes.txt', 'w+').write('\n'.join(map(str, primes)))
